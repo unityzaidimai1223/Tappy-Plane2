c4c63852d408f7d513ecea23becc6c0b6d8b3350
Simona <sima.simute1999@gmail.com>
2024-09-30 17:48:45 +0300

    Level design

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
